7d334a11b21ffff8e9c316ac95d8b1fb9a79c695
LENOVO <bendresakshi0@gmail.com>
2024-09-02 15:02:43 +0530

    Initial commit

---
+++ cruuddemo/src/main/java/com/sakshi/cruuddemo/dao/AppDAO.java
@@ -0,0 +1,39 @@
+package com.sakshi.cruuddemo.dao;
+
+import com.sakshi.cruuddemo.entity.Course;
+import com.sakshi.cruuddemo.entity.Instructor;
+import com.sakshi.cruuddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailsById(int theId);
+
+    List<Course> findCourseByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void  update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+
+
+}
+
